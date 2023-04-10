5b9a5c95feec92000edba33f5a27ebeb5db6dfd7
AvivaRubenstein <avivadrubenstein@gmail.com>
2023-04-10 17:19:48 -0400

    working on Portfolio and Project components to render 6 projects onto portfolio page

---
+++ src/components/Project.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+// Whenever we try to render an array containing JSX, React knows to render each JSX element separately
+function Project(props) {
+  return (
+    <ul className="list-group">
+      {/* Here we map over each grocery item and return a new array of `li` elements that contains the grocery name */}
+      {/* When using map you must provide a unique key attribute to each item. Ours is `item.id` */}
+      {props.projects.map(item => (
+        <li className="list-group-item" key={item.id}>
+          <h2>{item.title}</h2>
+          <img src={item.image} alt={item.alt}></img>
+          <h3>{item.github}</h3>
+          <h3>{item.link}</h3>
+          
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Project;
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,13 +1,66 @@
 import React from 'react';
 import Project from '../Project';
 
+const projects = 
+[
+{
+  id: 1,
+  title: "No Crude Food",
+  gitHub: "https://github.com/AlexWiederman/No_Crude_Food",
+  link: "https://young-mountain-95021.herokuapp.com/",
+  image: "https://github.com/AlexWiederman/No_Crude_Food/blob/main/public/assets/READMEimage1.png?raw=true",
+  alt: "Application Screenshot"
+},
+{
+  id: 2,
+  title: "Tech Blog",
+  gitHub: "https://github.com/AvivaRubenstein/MVC-Tech-Blog",
+  link: "https://radiant-everglades-93421.herokuapp.com/ ",
+  image: "https://user-images.githubusercontent.com/113466697/221442944-4496cb1c-8e20-46ce-ba51-a685d60bd0d5.png",
+  alt: "Application Screenshot"
+},
+{
+  id: 3,
+  title: "Educational Search Site",
+  gitHub: "https://github.com/crystal-coding-time/supreme-octo-system",
+  link: "https://crystal-coding-time.github.io/supreme-octo-system/",
+  image: "/Users/avivarubenstein/Documents/classwork/React-Portfolio/public/Screenshot 2023-04-10 at 5.04.10 PM.png",
+  alt: "Application Screenshot"
+},
+{
+  id: 4,
+  title: "Weather Dashboard",
+  gitHub: "https://github.com/AvivaRubenstein/weather-dashboard",
+  link: "https://avivarubenstein.github.io/weather-dashboard/",
+  image: "/Users/avivarubenstein/Documents/classwork/React-Portfolio/public/Screenshot 2023-04-10 at 5.07.09 PM.png",
+  alt: "Application Screenshot"
+},
+{
+  id: 5,
+  title: "Text Editor Progressive Web Application",
+  gitHub: "https://github.com/AvivaRubenstein/Text-Editor-PWA",
+  link: " https://nameless-wildwood-53462.herokuapp.com/ ",
+  image: "/Users/avivarubenstein/Documents/classwork/React-Portfolio/public/Screenshot 2023-04-10 at 5.09.13 PM.png",
+  alt: "Application Screenshot"
+},
+{
+  id: 6,
+  title: "Work Day Scheduler",
+  gitHub: "https://github.com/AvivaRubenstein/work-day-scheduler",
+  link: "https://avivarubenstein.github.io/work-day-scheduler/",
+  image: "/Users/avivarubenstein/Documents/classwork/React-Portfolio/public/Screenshot 2023-04-10 at 5.09.13 PM.png",
+  alt: "Application Screenshot"
+},
+]
+
+
 export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
       <p>
         Here we will return multiple Project components
-        <Project/>
+        <Project projects={projects}/>
       </p>
     </div>
   );
