f8a96ca4875d23db1c1c0571c3d8912ebe989874
AvivaRubenstein <avivadrubenstein@gmail.com>
2023-04-16 20:39:45 -0400

    Resume section text

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,9 +5,14 @@ export default function Resume() {
   return (
     <div>
       <h1>Resume</h1>
-      <p>
-        Here we will attach a copy of my resume, and list my technical skills in front and backend 
-      </p>
+      
+      <h3>Technical Skills</h3>
+      <ul>
+      <li>Frontend: HTML, CSS, JavaScript, JQuery, Bootstrap</li> 
+      <li>Backend: MySQL, MongoDB, Express.js, React.js, Node.js, Handlebars, Webpack</li>
+      </ul>
+      
+      
       {/* download attribute makes the target download when the link is clicked */}
       <a className='button btn btn-info' href={resume} download>
           Download My Resume
