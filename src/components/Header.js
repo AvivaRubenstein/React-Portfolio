71cd1345266af1d7f1b964c0bc34db3f374fa911
Aviva <avivadrubenstein@gmail.com>
2023-05-02 13:23:23 -0400

    set up React routing intead of using conditional rendering to display page sections, still need to format the header/navbar since using routing removes usage of Navbar.js component which was responsible for styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import Navbar from './Navbar';
+// import Navbar from './Navbar';
+import {Link} from 'react-router-dom';
 
 
-export default function Header({currentPage, handlePageChange}) {
+export default function Header() {
   return ( 
     <header>
-<Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+<Link to='/'>
+  <h1>
+    About Me
+  </h1>
+</Link>
+<Link to='/portfolio'>
+  <h1>
+    Portfolio
+  </h1>
+</Link>
+<Link to='/resume'>
+  <h1>
+    Resume
+  </h1>
+</Link>
+<Link to='/contact'>
+  <h1>
+    Contact
+  </h1>
+</Link>
+
     </header>
   );
 }
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Resume from './pages/Resume';
 import AboutMe from './pages/AboutMe';
 import Portfolio from './pages/Portfolio';
@@ -7,39 +8,33 @@ import Header from './Header';
 import Footer from './Footer'
 
 export default function PortfolioContainer() {
-  //currentPage is set to the About Me page
-  const [currentPage, setCurrentPage] = useState('AboutMe');
-  //we are passing in the currentPage we are on, which has the default value of being set to "About Me"
-  //and then we are using the currentPage we are on to return the relevant component 
-  const renderPage = () => {
-    //when currentPage === Resume, we pass in / render the Resume component
-    if (currentPage === 'Resume') {
-      return <Resume />;
-    }
-    if (currentPage === 'Contact') {
-      return <Contact />;
-    }
-    if (currentPage === 'Portfolio') {
-      return <Portfolio />;
-    }
-    return <AboutMe />;
-  };
-
-  //when we change pages, whatever page triggers the handlePageChange function will be passed
-  //in as the the new value that the currentPage is being set to
-  const handlePageChange = (page) => setCurrentPage(page);
-
   return (
-    // we are setting the minimum height of the columns/content to be 100% of the viewport, which means that the footer will be at the bottom of the page, not directly under content when content doesn't fill the page
-      //this is in conjunction with the footer's mt-auto class which gives the footer a top margin to fill the available space
-    <div className="d-flex flex-column min-vh-100">
-      {/* we are passing props into the NavTabs component:
-      the currentPage value is passed in as currentPage= 
-      and we are passing in the handlePageChange functionality to be able to update/set a new page */}
-      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* Here we are calling the renderPage method which will return a component */}
-      {renderPage()}
-      <Footer />
-    </div>
+<Router>
+<div className="flex-column justify-flex-start min-100-vh">
+  <Header/>
+  <div className="container">
+    <Routes>
+      <Route
+      path="/"
+      element={<AboutMe />}
+      />
+      <Route
+      path="/resume"
+      element={<Resume />}
+      />
+      <Route
+      path="/portfolio"
+      element={<Portfolio />}
+      />
+      <Route
+      path="/contact"
+      element={<Contact />}
+      />
+        
+    </Routes>
+  </div>
+  <Footer />
+  </div>
+</Router>
   );
 }
